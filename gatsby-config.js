6da4f506df99957433188883b014e6699543ab69
thomasgui <guilhem.thomas@gmail.com>
2020-08-05 13:28:09 +0200

    env

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: process.env.API_URL ||`http://localhost:1337`,
+        apiURL: process.env.GATSBY_API_URL ||`http://localhost:1337`,
         queryLimit: 1000, // Default to 100
         //   contentTypes : `jobs`, `projects`, `blogs`,
           // singleType : [`about`],
